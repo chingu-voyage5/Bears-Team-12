499d3031dbfa7004c62098c7608c570b88112475
sdbowen <sdbowen.git@gmail.com>
2018-06-09 13:05:02 -0500

    Add initial server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const mongoDB = 'mongodb://localhost/orderApp';
+
+mongoose
+  .connect(mongoDB)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
