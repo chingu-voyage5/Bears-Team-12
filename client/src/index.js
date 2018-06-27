38418f5d314910f127cfda766c5038846c32fd0a
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2018-06-28 01:25:14 +0800

    Setup Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 
+import store from './store'
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import './index.css';
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.querySelector('#root'));
