56fc7c684d86f27264463b28c31269abc5db952d
dontehightower <dontedhightower@gmail.com>
2018-07-06 21:40:36 -0500

    change a tags to Links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,3 @@
-// TODO: change a tags to links
-
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -10,24 +8,24 @@ class Navbar extends Component {
         <h2 className="navbar__logo">Feast</h2>
         <ul className="navbar__list">
           <li className="navbar__item">
-            <a className="navbar__link" href="/orders">
+            <Link className="navbar__link" to="/orders">
               Orders
-            </a>
+            </Link>
           </li>
           <li className="navbar__item">
-            <a href="/menu" className="navbar__link">
+            <Link to="/menu" className="navbar__link">
               Menu
-            </a>
+            </Link>
           </li>
           <li className="navbar__item">
-            <a href="/menu/new" className="navbar__link">
+            <Link to="/menu/new" className="navbar__link">
               Add Item
-            </a>
+            </Link>
           </li>
           <li className="navbar__item">
-            <a href="/drinks" className="navbar__link">
+            <Link to="/drinks" className="navbar__link">
               Drinks
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
